e0d81c9cafbe4f2a7235002d3696366344575cab
yourordinarycat <49221554+yourordinarycat@users.noreply.github.com>
2026-07-25 16:25:26 -0400

    Add more specific errors to manifest parsing

--- generator/src/manifest.rs
+++ generator/src/manifest.rs
@@ -1,7 +1,16 @@
 use serde::Deserialize;
-use std::fs;
 use std::convert::AsRef;
+use std::fs;
 use std::path::Path;
+
+#[derive(thiserror::Error, Debug)]
+pub enum ManifestError {
+    #[error(transparent)]
+    ParseError(#[from] serde_json::Error),
+
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+}
 
 #[derive(Deserialize, Debug)]
 pub struct Manifest {
@@ -12,7 +21,7 @@ pub struct Manifest {
     pub base_path: Option<String>,
 }
 
-pub fn load_manifest(src: impl AsRef<Path>) -> Result<Manifest, Box<dyn std::error::Error>> {
+pub fn load_manifest(src: impl AsRef<Path>) -> Result<Manifest, ManifestError> {
     let file_contents = fs::read_to_string(&src)?;
     let manifest: Manifest = serde_json::from_str(&file_contents)?;
 
