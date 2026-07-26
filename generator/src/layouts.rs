e9d2788dff625e8117f396d9a4b597b306c3e0fa
yourordinarycat <49221554+yourordinarycat@users.noreply.github.com>
2026-07-26 02:05:50 -0400

    Add diagnostics to layout discovery

--- generator/src/layouts.rs
+++ generator/src/layouts.rs
@@ -2,9 +2,20 @@ use dom_query::Document;
 use std::{collections::HashMap, fs, path::Path};
 use walkdir::WalkDir;
 
-use crate::directives::{
-    self, Directive, DirectiveContext, connection::CONNECTION_ID_ATTR, html::get_directive_context,
+use crate::{
+    diagnostics::Diagnostics,
+    directives::{
+        self, Directive, DirectiveContext, connection::CONNECTION_ID_ATTR,
+        html::get_directive_context,
+    },
+    path_ext,
 };
+
+#[derive(thiserror::Error, Debug)]
+pub enum LayoutDiscoveryError {
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+}
 
 #[derive(Debug)]
 pub struct Layout {
@@ -12,7 +23,7 @@ pub struct Layout {
     pub directives: HashMap<u8, (DirectiveContext, Vec<Directive>)>,
 }
 
-fn process_layout(content: &str) -> Result<Layout, Box<dyn std::error::Error>> {
+fn process_layout(content: &str) -> Layout {
     let document = Document::from(content);
     let mut directive_map: HashMap<u8, (DirectiveContext, Vec<Directive>)> = HashMap::new();
     let mut curr_cid: u8 = 0;
@@ -31,36 +42,46 @@ fn process_layout(content: &str) -> Result<Layout, Box<dyn std::error::Error>> {
         }
     }
 
-    Ok(Layout {
+    Layout {
         content: document.html().to_string(),
         directives: directive_map,
-    })
+    }
 }
 
-pub fn get_layouts(src: impl AsRef<Path>) -> Result<HashMap<String, Layout>, Box<dyn std::error::Error>> {
+fn process_entry(
+    map: &mut HashMap<String, Layout>,
+    entry: Result<walkdir::DirEntry, walkdir::Error>,
+) -> Result<(), LayoutDiscoveryError> {
+    let entry = entry.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+    if entry.file_type().is_dir() {
+        return Ok(());
+    }
+
+    let src_path = entry.path();
+    let content = fs::read_to_string(&src_path)?;
+
+    let name = path_ext::name_to_str(src_path.file_stem())?;
+
+    let layout = process_layout(&content);
+    map.insert(name.to_owned(), layout);
+
+    Ok(())
+}
+
+pub fn get_layouts(src: impl AsRef<Path>) -> (HashMap<String, Layout>, Diagnostics) {
+    let mut diagnostics = Diagnostics::default();
     let mut map: HashMap<String, Layout> = HashMap::new();
 
     let src = src.as_ref();
 
     for entry in WalkDir::new(src) {
-        let entry = entry.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-        if entry.file_type().is_dir() {
-            continue;
-        }
-
-        let src_path = entry.path();
-        let content = fs::read_to_string(&src_path)?;
-
-        let name = entry
-            .path()
-            .file_stem()
-            .expect("File stem should be present.")
-            .to_str()
-            .expect("Failed to convert file stem to string.");
-
-        let layout = process_layout(&content)?;
-        map.insert(name.to_string(), layout);
+        match process_entry(&mut map, entry) {
+            Ok(_) => (),
+            Err(err) => {
+                diagnostics.errors.push(err.into());
+            }
+        };
     }
 
-    Ok(map)
+    (map, diagnostics)
 }
